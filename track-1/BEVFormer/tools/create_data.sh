c9e074e09a9786eb1298c6b72f9345a59091db20
web3hayden <wanghao@web3.com>
2024-01-14 13:53:52 +0800

    打5

--- track-1/BEVFormer/tools/create_data.sh
+++
@@ -1,9 +0,0 @@
-cp -r ./data/robodrive-release/robodrive-v1.0-test ./data/nuscenes
-
-python tools/create_data.py \
-    nuscenes \
-    --root-path ./data/nuscenes \
-    --out-dir ./data/nuscenes \
-    --extra-tag robodrive \
-    --version robodrive-v1.0 \
-    --canbus ./data/nuscenes
